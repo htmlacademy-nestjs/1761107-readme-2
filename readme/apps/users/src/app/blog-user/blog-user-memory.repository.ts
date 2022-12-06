811f06ac88c0549831fea85e6c647cb9e7ddc331
Anna Markelova <anytkamarkelova@yandex.ru>
2022-12-06 13:10:22 +0700

    feat(Users) adds BlogUserMemoryRepository

---
+++ readme/apps/users/src/app/blog-user/blog-user-memory.repository.ts
@@ -0,0 +1,30 @@
+import { CRUDRepository } from '@readme/core';
+import { User } from '@readme/shared-types';
+import { BlogUserEntity } from './blog-user.entity';
+
+export class BlogUserMemoryRepository implements CRUDRepository<BlogUserEntity, string, User> {
+  private repository: { [key: string]: User } = {};
+
+  public async findById(id: string): Promise<User> {
+    if (this.repository[id]) {
+      return { ...this.repository[id] }
+    }
+    return null
+  }
+
+  public async create(item: BlogUserEntity): Promise<User> {
+    const entry = { ...item.toObject(), _id: crypto.randomUUID()};
+    this.repository[entry._id] = entry;
+
+    return {...entry};
+  }
+  public async update(id: string, item: BlogUserEntity): Promise<User> {
+    this.repository[id] = {...item.toObject(), _id: id};
+    return this.findById(id);
+  }
+
+  public async destroy(id: string): Promise<void> {
+    delete this.repository[id];
+  }
+
+}
