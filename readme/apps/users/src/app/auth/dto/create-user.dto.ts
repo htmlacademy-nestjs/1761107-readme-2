5db813c2c221ca721023113862c18ff027833d36
Anna Markelova <anytkamarkelova@yandex.ru>
2022-12-06 23:21:07 +0700

    feat(Users) adds swagger description

--- readme/apps/users/src/app/auth/dto/create-user.dto.ts
+++ readme/apps/users/src/app/auth/dto/create-user.dto.ts
@@ -1,5 +1,22 @@
+import {ApiProperty} from '@nestjs/swagger';
+
 export class CreateUserDto {
+
+  @ApiProperty({
+    description: 'User unique address',
+    example: 'user@user.ru'
+  })
   email: string;
+
+  @ApiProperty({
+    description: 'User login',
+    example: 'Keks',
+  })
   login: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: '123456'
+  })
   password: string;
 }
--- readme/apps/users/src/app/auth/rdo/user.rdo.ts
+++ readme/apps/users/src/app/auth/rdo/user.rdo.ts
@@ -1,12 +1,26 @@
 import {Expose} from 'class-transformer';
+import {ApiProperty} from '@nestjs/swagger';
 
 export class UserRdo {
+
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '13'
+  })
   @Expose({ name: '_id'})
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local'
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'User login',
+    example: 'Keks'
+  })
   @Expose()
   public login: string;
 
