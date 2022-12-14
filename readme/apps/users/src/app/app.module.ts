fab9d4ba3cb3c750727d702f7133465f29cd4054
Anna Markelova <anytkamarkelova@yandex.ru>
2022-12-14 16:35:28 +0100

    feat(users) adds validation environment

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_FILE_PATH } from './app.constant';
 import databaseConfig from '../config/database.config';
+import { validateEnvironments } from './env.validation';
+// import envSchema from './env.schema';
 
 @Module({
   imports: [
@@ -11,7 +13,9 @@ import databaseConfig from '../config/database.config';
       cache: true,
       isGlobal: true,
       envFilePath: ENV_FILE_PATH,
-      load: [databaseConfig]
+      load: [databaseConfig],
+      validate: validateEnvironments,
+      // validationSchema: envSchema alternative decision
     }),
     AuthModule,
     BlogUserModule
