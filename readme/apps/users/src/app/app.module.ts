eee7310758e8282553643242eadb54e45cacdfbf
Anna Markelova <anytkamarkelova@yandex.ru>
2022-12-14 14:18:21 +0100

    feat(readme) connects config to module

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from './app.constant';
+import databaseConfig from '../config/database.config';
 
 @Module({
-  imports: [BlogUserModule, AuthModule ],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig]
+    }),
+    AuthModule,
+    BlogUserModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
