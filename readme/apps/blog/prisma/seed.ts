6a4ba8cb0eebdca1adaaa0c56f6de904cf4d96ef
Anna Markelova <anytkamarkelova@yandex.ru>
2022-12-27 12:47:59 +0100

    adds seed

---
+++ readme/apps/blog/prisma/seed.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function fillDb() {
+
+  await prisma.post.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      userId: '123',
+      type: 'Text',
+      isRepost: false,
+      tags: ['#test'],
+      titlePost: 'bla',
+      previewPost: 'bla bla',
+      textPost: 'bla bla bla',
+      comments: {
+        create: [
+          {
+            text: 'bla bla bla',
+            userId: '111'
+          },
+        ]
+      },
+    }
+  });
+
+  console.info('🤘️ Database was filled')
+}
+
+fillDb()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect()
+
+    process.exit(1);
+  })
